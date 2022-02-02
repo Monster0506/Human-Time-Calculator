e89e67dd11265c3ef1b7f26a64983713861fc0d4
Monster0506 <91501317+Monster0506@users.noreply.github.com>
2022-02-02 07:24:05 -0500

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,7 @@
+# Determine type to convert from.
+typ = input("seconds or milliseconds? (s/[ms])  ")
+
+if typ=="s":
+  num = int(input("Ok. Converting seconds to human readable format.\nPlease Enter the number of seconds.  "))
+ else:
+  num=int(input("Ok. Converting milliseconds to human readable format.\nPlease Enter the number of milliseconds.  "))
